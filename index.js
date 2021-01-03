6e48f057c30bcca3d3a2a32a92d90b7508f883aa
51964 <juan.cotrina@pucp.pe>
2021-01-02 20:53:31 -0500

    cambiamos el estilo del boton ayudados del localsotage

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 function myFunction(){
     const btn = document.getElementById('btn');
-    btn.textContent="me has hecho click";
-    btn.style.color="white";
-    localStorage.setItem('btn',btn.textContent);
+    if (localStorage.getItem('btn')==="me has hecho click"){
+        btn.textContent="no me has hecho click";
+        btn.style.background="green";
+        btn.style.color="white";
+        localStorage.setItem('btn',btn.textContent);
+    } else {
+        btn.textContent="me has hecho click";
+        btn.style.color="white";
+        btn.style.background="red";
+        localStorage.setItem('btn',btn.textContent);
+    }
+    
 }
