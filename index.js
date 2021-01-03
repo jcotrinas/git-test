6dbbcc29d1a285661b19a6f51f501156ab819cf4
51964 <juan.cotrina@pucp.pe>
2021-01-03 12:28:42 -0500

    agrega funcion forEach

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ function myFunction(){
 1) localStorage guarda incluso cuando se cierran las pestañas del navegador, incluso cuando se reinicia la máquina.
 2) es accesible siempre en cuando se tenga el mismo origen: a) mismo protocolo, b) mismo dominio, c) mismo puerto. La URL puede ser diferente.
 c) métodos localStorage y sessionStorage: setItem(key, value) getItem(key), removeItem(key), clear() <borra todo>.
+3) sessionStorage sobrevive a la actualización de la página (pero no al cierre de pestaña)
 */
 
 function getNombre(){
@@ -26,4 +27,10 @@ function getNombre(){
     // luego podemos recuperar
     let user = JSON.parse( sessionStorage.user );
     alert( user.name ); // Juan
+}
+
+function arrayforEach(){
+    const btn3= document.querySelector('#btn3');
+    const array1 = ['Juan', 'Lucas', 'Pedro', 'María'];
+    array1.forEach(e => alert(e));
 }
